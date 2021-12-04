73b3535b81bacaca8a805cc07134a30fdeffd01d
vinod-r <vinod2x93@gmail.com>
2021-12-04 00:17:17 -0500

    all features and content implemented.

--- src/components/PortfolioPiece.js
+++ src/components/PortfolioPiece.js
@@ -4,6 +4,12 @@ import "../styles/PortfolioPiece.scss";
 import Plc from "../icons/plcholder.jpg";
 import { useSetRecoilState } from "recoil";
 import { navHoverCheckState } from "../store/atoms";
+import headballKey from "../images/headball.png";
+import headball1 from "../images/headball_1.png";
+import headball3 from "../images/headball_3.png";
+import snaccKey from "../images/snacc.png";
+import snacc1 from "../images/snacc_1.png";
+import snacc2 from "../images/snacc_2.png";
 
 export const PortfolioPiece = () => {
   //declaring refs, recoil states and parameters of page (to identify which project to display)
@@ -62,51 +68,56 @@ export const PortfolioPiece = () => {
     headball: {
       title: "Headball",
       subtitle: "A Persuasive Game",
-      keyImage: "",
-      roleInfo: "Designer",
-      timelineInfo: "2 Weeks",
-      skillsInfo: "Design, Strategy, Research",
+      keyImage: headballKey,
+      roleInfo: "Researcher Designer",
+      timelineInfo: "3.5 Months",
+      skillsInfo: "Game Design Visual Design Interviewing Motion Design",
       brief:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error deleniti aliquid magni facilis quibusdam similique aspernatur. Tempore cum dignissimos earum.",
+        "Despite the growing attention and major names like Michael Phelps and Simon Biles speaking about their mental health issues, many student-athletes still struggle with finding the right resources to cope with the stressors in their day to day life.",
       insights:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error deleniti aliquid magni facilis quibusdam similique aspernatur. Tempore cum dignissimos earum.",
-      insightsImage: "",
+        "Our research revealed that having a strong relationship with their team and being comfortable enough to share their problems and struggles, especially about mental health, played a critical role in helping college athletes cope with anxiety, stress, and fear while recovering from injuries.",
+      insightsImage: headball1,
       problem:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error deleniti aliquid magni facilis quibusdam similique aspernatur. Tempore cum dignissimos earum.",
-      problemImage: "",
+        "The problem to be tackled was the stigma around mental health issues in sports, and unjustified expectations that athletes need to have a ‘strong mentality’ and can’t be ‘weak.’ A statistic to address and affect was that only 10% of college athletes with mental health issues sought help.",
       solution:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error deleniti aliquid magni facilis quibusdam similique aspernatur. Tempore cum dignissimos earum.",
-      solutionImage: "",
+        "Headball is a game that aims to help student athletes foster relationships with their teammates and engender an environment where they can confidently seek support. Played in teams, it uses a randomly generated game board that provides players the option to choose paths that involve one of three minigames. These paths can either be high-risk high-reward or safe routes to create an element of strategy and teamwork.",
+      solutionImage: headball3,
     },
     snacc: {
       title: "Snacc on PennMac",
       subtitle: "A Service Innovation",
-      keyImage: "",
-      roleInfo: "Designer",
-      timelineInfo: "2 Weeks",
-      skillsInfo: "Design, Strategy, Research",
-      brief: "LoremIpsum",
-      insights: "LoremIpsum",
-      insightsImage: "",
-      problem: "LoremIpsum",
-      problemImage: "",
-      solution: "LoremIpsum",
-      solutionImage: "",
+      keyImage: snaccKey,
+      roleInfo: "Designer Researcher",
+      timelineInfo: "2 Months",
+      skillsInfo: "Service Design Business Strategy Research",
+      brief:
+        "Pittsburgh's population is on the decline, and the Pennsylvania Macaroni Company, a 120 year old institution in the heart of Pittsburgh, needs new blood to keep it's business running. How can we bring new and young families to PennMac and forge new traditions?",
+      insights:
+        "Since the end of the steel era in the late 60s, Pittsburgh's population has declined from a high of near 700,000 to just under 300,000 in 2021. This matches up to the population of the city in 1902 when PennMac was founded. Most of the customers we interviewed said that they visit PennMac since that was what they did as a child or they married into a family that had that tradition. New Millennial Tech Workers moving into Pittsburgh do not have this tradition.",
+      insightsImage: snacc1,
+      problem:
+        "PennMac faces the threat of commercial decline if they do not tap into a new population demographic. Previous attempts at expanded business operations (wholesaling, delivery, etc.) were not successful. But without an injection of new ideas, the business model faces the risk of growing stale and falling behind.",
+      solution:
+        "Our solution was a cooking demonstration with a partner catering company at the campus of a tech company to bring young millennial techies into the PennMac fold. Receiving quality ingredients and recipies from PennMac via an easy QR code, young families can experience the culture and tradition of Pittsburgh's oldest families in their homes.",
+      solutionImage: snacc2,
     },
     placeholder: {
       title: "Placeholder",
       subtitle: "Probably a cool project",
-      keyImage: "",
+      keyImage: Plc,
       roleInfo: "Designer",
-      timelineInfo: "2 Weeks",
+      timelineInfo: "To be decided",
       skillsInfo: "Design, Strategy, Research",
-      brief: "LoremIpsum",
-      insights: "LoremIpsum",
-      insightsImage: "",
-      problem: "LoremIpsum",
-      problemImage: "",
-      solution: "LoremIpsum",
-      solutionImage: "",
+      brief:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium et alias aspernatur! Dolore similique modi itaque quod vero porro alias.",
+      insights:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, in temporibus. Ea magni porro esse magnam doloremque ratione incidunt aliquid.",
+      insightsImage: Plc,
+      problem:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda minus, quo quia delectus id rem est nemo expedita ratione odio.",
+      solution:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab delectus eius repellat ipsa recusandae reprehenderit culpa nam eaque assumenda cumque.",
+      solutionImage: Plc,
     },
   };
   const project = projectList[projectName];
@@ -114,6 +125,7 @@ export const PortfolioPiece = () => {
   return (
     <>
       <div
+        ref={scrollDownRef}
         className="scroll-down"
         onClick={() => {
           window.scrollTo(0, 800);
@@ -126,7 +138,7 @@ export const PortfolioPiece = () => {
           </g>
         </svg>
       </div>
-      <div className="project-content" ref={scrollDownRef}>
+      <div className="project-content">
         <button
           ref={backButtonRef}
           className="back-button"
@@ -157,7 +169,7 @@ export const PortfolioPiece = () => {
           {project.subtitle}
         </h1>
         <img
-          src={Plc}
+          src={project.keyImage}
           alt="key"
           className="project-image"
           data-aos="fade-up"
@@ -184,7 +196,7 @@ export const PortfolioPiece = () => {
             {project.insights}
           </p>
           <img
-            src={Plc}
+            src={project.insightsImage}
             alt=""
             className="project-insights-image project-slab-image"
           />
@@ -194,11 +206,6 @@ export const PortfolioPiece = () => {
           <p className="project-problem-text  project-slab-text">
             {project.problem}
           </p>
-          <img
-            src={Plc}
-            alt=""
-            className="project-problem-image project-slab-image"
-          />
         </div>
         <div className="project-solution project-slab" data-aos="fade-up">
           <div className="project-slab-title">Solution</div>
@@ -206,7 +213,7 @@ export const PortfolioPiece = () => {
             {project.solution}
           </p>
           <img
-            src={Plc}
+            src={project.solutionImage}
             alt=""
             className="project-solution-image project-slab-image"
           />
